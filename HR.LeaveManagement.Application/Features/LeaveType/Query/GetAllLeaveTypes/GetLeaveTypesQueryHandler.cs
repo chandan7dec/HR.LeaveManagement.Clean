a8f2d5c979813892039f18ce09acf7bf0302c7f4
chandan7dec <ck.kislay@gmail.com>
2025-05-02 12:07:01 +0530

    1/5/2025

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Query/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistance;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Query.GetAllLeaveTypes
+{
+    public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public GetLeaveTypesQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+           _mapper = mapper;
+           _leaveTypeRepository = leaveTypeRepository;
+        }
+        public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+        {
+
+            //query to database
+            var leaveType =await  _leaveTypeRepository.GetAsync();
+
+            //convert data object to dto object
+            var data = _mapper.Map<List<LeaveTypeDto>>(leaveType);
+
+            //return list of dto object
+            return data;
+        }
+    }
+}
--- HR.LeaveManagement.Domain/LeaveType.cs
+++ HR.LeaveManagement.Domain/LeaveType.cs
@@ -8,9 +8,9 @@ using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Domain
 {
-    public class LeaveType
+    public class LeaveType : BaseEntity
     {
-        public int Id { get; set; }
+       
         public string Name { get; set; } = string.Empty;
         public int  DefaultDays { get; set; }
 
